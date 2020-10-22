a50e95856269f6b5c624a672367bffbd7427283b
margittennosaar <46651865+margittennosaar@users.noreply.github.com>
2020-10-22 14:44:53 +0300

    first task solution

--- 1_boxes/src/App.js
+++ 1_boxes/src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Box from "./Components/Box/Box";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box name="Maria" title="CEO" age="39" />
+      <Box name="Kati" title="Developer" age="20" />
+      <Box name="Karin" title="Desinger" age="42" />
     </div>
   );
 }
