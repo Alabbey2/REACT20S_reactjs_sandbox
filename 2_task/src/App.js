6bf35b6f889c4483896617e17e14198a9a6fe88f
margittennosaar <46651865+margittennosaar@users.noreply.github.com>
2020-10-29 13:04:20 +0200

    animal cards with props and button

--- 2_task/src/App.js
+++ 2_task/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Animal from "./Components/Animal/Animal";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="cards">
+      <Animal name="Fox" img="https://source.unsplash.com/AjZjBEjQ5Cw/" />
+      <Animal name="Rabbit" img="https://source.unsplash.com/hS41iEO300E/" />
+      <Animal name="Wolf" img="https://source.unsplash.com/WFPWB7Vum1E/" />
     </div>
   );
 };
