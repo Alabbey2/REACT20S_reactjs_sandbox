8a8b8ad984f38c30285a20aa04a85b7d91147d91
margittennosaar <46651865+margittennosaar@users.noreply.github.com>
2020-11-09 12:56:11 +0200

    added form example to 4_task

--- 4_task/src/Components/Main/Main.js
+++ 4_task/src/Components/Main/Main.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Article from "../Article/Article";
+import Form from "../Form/Form";
 
 const Main = () => {
   return (
     <main>
-      <Article />
+      <Form />
     </main>
   );
 };
